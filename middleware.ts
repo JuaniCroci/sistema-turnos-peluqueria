b39ec89597079906c2b2a23d62e67edc707bbc7e
juani Croci <juanicroci.jc@gmail.com>
2026-06-16 14:58:10 -0300

    plans/README.md: mark plan 012 DONE

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import NextAuth from 'next-auth';
-import { authEdgeConfig } from '@/lib/auth/config.edge';
-
-export const { auth: middleware } = NextAuth(authEdgeConfig);
-
-export const config = {
-  matcher: ['/mis-turnos/:path*', '/admin/:path*'],
-};
